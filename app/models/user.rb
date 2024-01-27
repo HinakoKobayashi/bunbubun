9187107e1e3a008434d35ae47917c3937491a8de
HinakoKobayashi <144582159+HinakoKobayashi@users.noreply.github.com>
2024-01-27 23:35:17 +0900

    ユーザーモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+
+  has_many :select_answers, dependent: :destroy
 end
